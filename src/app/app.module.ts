56d89e4441d5a4d675d339b901f517b29bd3c801
Julius Platon <j.platon@tu-bs.de>
2022-04-12 16:38:59 +0200

    New Styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DelayDirective } from './directives/delay.directive';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatInputModule} from '@angular/material/input';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
@@ -27,7 +28,8 @@ import {MatButtonModule} from '@angular/material/button';
     ReactiveFormsModule, 
     BrowserAnimationsModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
